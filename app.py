b171bc692a1159a11c2d26cfed2cdf503c4d586e
= <really.eli@gmail.com>
2016-07-02 12:21:49 -0400

    Added tests.py, moved optimal move functionality into the main app.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 app = Flask(__name__)
 
 def is_valid(board):
-    # Checks both whether the board is a valid representation of a tictacoe board, and whether it could be o's turn.
+    """ Checks both whether the board is a valid representation of a tictacoe board, and whether it could be o's turn. """
     return re.match(r'[o x]{9}', board) and (board.count("x") - board.count("o") in [0, 1])
 def is_tie(board):
     return not (" " in board)
@@ -26,17 +26,33 @@ def is_winner(board, player):
     ]
     return any(horizontals) or any(verticals) or any(diagonals)
 
+def other_player(player):
+    return {'o': 'x', 'x': 'o'}[player]
 def move(board, index, player):
     splits = list(board)
     splits[index] = player
     return "".join(splits)
 def candidate_boards(board, player):
     return [move(board, i, player) for i, char in enumerate(board) if char == " "]
+def score(board, player):
+    """ Returns -1 if player will lose on this board, 0 if player can tie, and 1 if player can win. """
+    opp = other_player(player)
+    if is_winner(board, player):
+        return 1
+    if is_winner(board, opp):
+        return -1 
+    if is_tie(board):
+        return 0
+
+    candidates = candidate_boards(board, player)
+    return max(-1 * score(candidate, opp) for candidate in candidates)
 
 @app.route('/')
 def main():
-    board = request.args.get('board', '')
-    print is_valid(board)
-    if not is_valid(board) or is_tie(board) or is_winner(board, 'x') or is_winner(board, 'o'):
+    board = request.args.get('board', None)
+    if board is None or not is_valid(board) or is_tie(board) or is_winner(board, 'x') or is_winner(board, 'o'):
        abort(400) 
-    return candidate_boards(board, player='o')[0]
+
+    candidates = candidate_boards(board, 'o')
+    return max(candidates, key=lambda candidate: -1 * score(candidate, 'x'))
+    # return candidate_boards(board, player='o')[0]
---
+++ tests.py
@@ -0,0 +1,71 @@
+import unittest
+import app
+from app import score
+
+class TestingBadRequests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_400_on_ill_formatted_board(self):
+        r = self.app.get('/', query_string="board=AAA")
+        self.assertEqual(r.status_code, 400)
+
+    def test_400_on_no_board_provided(self):
+        r = self.app.get('/', query_string="bored=xx oo xox")
+        self.assertEqual(r.status_code, 400)
+        
+    def test_400_on_full_board(self):
+        r = self.app.get('/', query_string="board=xxxoooxox")
+        self.assertEqual(r.status_code, 400)
+
+    def test_400_on_winning_board(self):
+        r = self.app.get('/', query_string="board=xxx   ooo")
+        self.assertEqual(r.status_code, 400)
+
+    def test_400_on_not_plausibly_os_turn(self):
+        r = self.app.get('/', query_string="board=xxx      ")
+        self.assertEqual(r.status_code, 400)
+
+class TestingGoodRequests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_200_on_blank_board(self):
+        r = self.app.get('/', query_string="board=         ")
+        self.assertEqual(r.status_code, 200)
+
+class TestingOptimality(unittest.TestCase):
+    def test_score_winning_and_losing_base_cases(self):
+        self.assertEqual(score('xxx      ', player='x'), 1)
+        self.assertEqual(score('xxx      ', player='o'), -1)
+
+        self.assertEqual(score('o  o  o  ', player='o'), 1)
+        self.assertEqual(score('o  o  o  ', player='x'), -1)
+        self.assertEqual(score('x  x  x  ', player='x'), 1)
+        self.assertEqual(score('x  x  x  ', player='o'), -1)
+
+    def test_score_tie_base_cases(self):
+        self.assertEqual(score('xoooxxoxo', player='x'), 0)
+        self.assertEqual(score('xoooxxoxo', player='o'), 0)
+
+    def test_score_easy_board_can_win(self):
+        self.assertEqual(score('oo xx    ', player='o'), 1)
+        self.assertEqual(score('oo xx    ', player='x'), 1)
+
+    @unittest.skip("takes a long time")
+    def test_score_blank_board_is_tie(self):
+        self.assertEqual(score('         ', player='o'), 0)
+        self.assertEqual(score('         ', player='x'), 0)
+
+class IntegrationTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_easy_board(self):
+        # Lincoln's example given in the google doc.
+        r = self.app.get('/', query_string="board= xxo  o  ")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.get_data(), 'oxxo  o  ')
+
+if __name__ == "__main__":
+    unittest.main()
